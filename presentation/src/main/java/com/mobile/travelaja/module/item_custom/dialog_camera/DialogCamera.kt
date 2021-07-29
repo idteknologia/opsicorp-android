1d3f7e947f756a8ab4927434ba9b162e0083d41b
bdgit08 <dior.tantri@gmail.com>
2021-07-29 15:30:06 +0700

    Bug fix Attachment

--- presentation/src/main/java/com/mobile/travelaja/module/item_custom/dialog_camera/DialogCamera.kt
+++ presentation/src/main/java/com/mobile/travelaja/module/item_custom/dialog_camera/DialogCamera.kt
@@ -12,27 +12,32 @@ import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.os.ParcelFileDescriptor
 import android.os.StrictMode
 import android.provider.MediaStore
+import android.provider.OpenableColumns
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.FileProvider
 import com.mobile.travelaja.R
 import com.mobile.travelaja.utility.Globals
 import com.unicode.kingmarket.Base.BaseDialogFragment
 import com.yalantis.ucrop.UCrop
 import kotlinx.android.synthetic.main.dialog_camera.*
-import java.io.File
-import java.io.IOException
+import java.io.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class DialogCamera : BaseDialogFragment() {
-    override fun getLayout(): Int { return R.layout.dialog_camera }
+    override fun getLayout(): Int {
+        return R.layout.dialog_camera
+    }
 
-    protected val CAMERA_REQUEST  = 0
+    protected val CAMERA_REQUEST = 0
     protected val GALLERY_PICTURE = 1
     lateinit var imgFile: File
-    var pictureImagePath          = ""
-    lateinit var callbackDialog   : DialogCameraCallback
+    var pictureImagePath = ""
+    lateinit var callbackDialog: DialogCameraCallback
 
     override fun onMain(fragment: View, savedInstanceState: Bundle?) {
         setCornerDialog()
@@ -41,22 +46,22 @@ class DialogCamera : BaseDialogFragment() {
 
     private fun setInitClickListener() {
         line_upload_file.setOnClickListener {
-            if(line_upload_file.background.constantState == resources.getDrawable(R.drawable.rounded_button_camera).constantState){
+            if (line_upload_file.background.constantState == resources.getDrawable(R.drawable.rounded_button_camera).constantState) {
                 getImageFromGalery()
-            }
-            else {
-                callbackDialog.data(pictureImagePath,imgFile)
+//                openDirectory()
+            } else {
+                callbackDialog.data(pictureImagePath, imgFile)
                 dismiss()
             }
         }
         btn_camera1.setOnClickListener {
-            getOpenCamera()
+            dispatchTakePicture()
         }
         btn_camera2.setOnClickListener {
-            getOpenCamera()
+            dispatchTakePicture()
         }
         image_selected.setOnClickListener {
-            getOpenCamera()
+            dispatchTakePicture()
         }
     }
 
@@ -66,13 +71,12 @@ class DialogCamera : BaseDialogFragment() {
         }
     }
 
-    fun changeButtonUploaded(){
-        if (line_upload_file.background.constantState==resources.getDrawable(R.drawable.rounded_button_camera).constantState){
+    fun changeButtonUploaded() {
+        if (line_upload_file.background.constantState == resources.getDrawable(R.drawable.rounded_button_camera).constantState) {
             line_upload_file.setBackgroundDrawable(resources.getDrawable(R.drawable.rounded_button_camera_uploaded))
             img_upload_file.visibility = View.GONE
             title_upload_file.setTextColor(resources.getColor(R.color.white))
-        }
-        else {
+        } else {
             line_upload_file.background = resources.getDrawable(R.drawable.rounded_button_camera)
             title_upload_file.setTextColor(resources.getColor(R.color.colorDarkGrayRound))
             img_upload_file.visibility = View.VISIBLE
@@ -81,50 +85,50 @@ class DialogCamera : BaseDialogFragment() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
-        when(requestCode){
+        when (requestCode) {
             CAMERA_REQUEST -> {
                 if (resultCode == Activity.RESULT_OK) {
-                    changeButtonUploaded()
-                    setImage(pictureImagePath)
+                    setImageFileFromCamera()
+//                  setImage(pictureImagePath)
                 }
             }
-
             GALLERY_PICTURE -> {
                 if (resultCode == Activity.RESULT_OK) {
                     try {
-                        changeButtonUploaded()
-                        val selectedImage = data!!.data
-                        if (selectedImage != null) {
-                            if (selectedImage.path!!.contains("/raw/")){
-                                pictureImagePath = selectedImage.path.toString().replace("/raw/","")
+                        data?.data?.let { uri ->
+                            val path = uri.path
+                            println(path)
+                            if (uri.path != null && uri.path!!.contains("/raw/")) {
+                                pictureImagePath = uri.path.toString().replace("/raw/", "")
                             } else {
-                                pictureImagePath  = selectedImage.path!!
+                                showFileGallery(uri)
+                            }
+                            if (pictureImagePath.contains("pdf") || pictureImagePath.contains("doc") || pictureImagePath.contains(
+                                    "xls"
+                                ) || pictureImagePath.contains("xlsx")
+                            ) {
+                                callbackDialog.data(pictureImagePath, File(pictureImagePath))
+                                dismiss()
                             }
                         }
-                        if (pictureImagePath.contains("jpg")||pictureImagePath.contains("png")||pictureImagePath.contains("jpeg")){
-                            val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, selectedImage)
-                            image_selected.setImageBitmap(bitmap)
-                        }
-                        if (pictureImagePath.contains("pdf")||pictureImagePath.contains("doc")||pictureImagePath.contains("xls")||pictureImagePath.contains("xlsx")){
-                            callbackDialog.data(pictureImagePath, File(getPath(selectedImage)))
-                            dismiss()
-                        }
-                        imgFile = File(getPath(selectedImage))
                     } catch (e: IOException) {
                         Globals.setLog("TAG", "Some exception $e")
                     }
 
                 }
             }
-
         }
 
+        if (pictureImagePath.isNotEmpty()){
+            changeButtonUploaded()
+        }
     }
+
 
     fun getPath(uri: Uri?): String? {
         val projection = arrayOf(MediaStore.Images.Media.DATA)
-        val cursor: Cursor = context?.getContentResolver()?.query(uri!!, projection, null, null, null) ?: return null
+        val cursor: Cursor =
+            context?.getContentResolver()?.query(uri!!, projection, null, null, null) ?: return null
         val column_index: Int = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
         cursor.moveToFirst()
         val s: String = cursor.getString(column_index)
@@ -143,9 +147,178 @@ class DialogCamera : BaseDialogFragment() {
     }
 
     private fun getImageFromGalery() {
-        val type = "*/*"//"image/*|application/pdf|application/doc/|application/docx/|application/xls/|application/xlsx/*"
-        val pictureActionIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI).setType(type) // */* setType("image/*")
-        startActivityForResult(Intent.createChooser(pictureActionIntent, "Select File"),GALLERY_PICTURE)
+//        val type = "*/*"//"image/*|application/pdf|application/doc/|application/docx/|application/xls/|application/xlsx/*"
+        val pictureActionIntent = Intent(Intent.ACTION_PICK).apply {
+            type = "*/*"
+        } // */* setType("image/*")
+        startActivityForResult(pictureActionIntent, GALLERY_PICTURE)
+    }
+
+//    fun openDirectory() {
+//        // Choose a directory using the system's file picker.
+//        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply {
+//            // Provide read access to files and sub-directories in the user-selected
+//            // directory.
+//            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+//
+//            // Optionally, specify a URI for the directory that should be opened in
+//            // the system file picker when it loads.
+//            putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+//        }
+//
+//        startActivityForResult(intent, GALLERY_PICTURE)
+//    }
+
+    //Todo show file from Gallery
+    private fun showFileGallery(uri: Uri) {
+        val inputPfd: ParcelFileDescriptor?
+        var size = 0L
+        var name = ""
+        var compress = 100
+        try {
+            inputPfd = context?.contentResolver?.openFileDescriptor(uri, "r")
+            val type = context?.contentResolver?.getType(uri)
+            if (type.isNullOrEmpty()) {
+                return
+            }
+            context?.contentResolver?.query(uri, null, null, null, null).use { cursor ->
+                val s = cursor?.getColumnIndex(OpenableColumns.SIZE)
+                val n = cursor?.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                cursor?.moveToFirst()
+                if (s != null && n != null) {
+                    size = cursor.getLong(s) / (1024 * 1024)
+                    name = cursor.getString(n)
+                    compress = getCompressSize(size)
+                }
+
+                val fd = inputPfd?.fileDescriptor ?: return
+                val imageFolder = File(context?.cacheDir, "Opsicorp")
+                if (!imageFolder.exists()) {
+                    imageFolder.mkdir()
+                }
+                val imgFile = File(imageFolder, "opsicorp-$name")
+                val fOut = FileOutputStream(imgFile)
+
+                if (!type.contains("image")) {
+                    if (size>1){
+                        Toast.makeText(
+                            requireContext(),
+                            getString(R.string.txt_failed_upload_file_size_is_to_large),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        return
+                    }
+                    val input = FileInputStream(fd)
+                    var read: Int
+                    val bytes = ByteArray(4096)
+                    while ((input.read(bytes).also { read = it }) != -1) {
+                        fOut.write(bytes, 0, read)
+                    }
+                    pictureImagePath = imgFile.absolutePath
+                    return
+                }
+
+                if (compress == -1){
+                    return
+                }
+                val bitmap = getBitmapImage(fd)
+                image_selected.setImageBitmap(bitmap)
+                bitmap?.compress(Bitmap.CompressFormat.JPEG, compress, fOut)
+                fOut.flush()
+                fOut.close()
+                this.imgFile = imgFile
+                this.pictureImagePath = imgFile.absolutePath
+            }
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+            return
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun getBitmapImage(fd: FileDescriptor): Bitmap? {
+        val b = BitmapFactory.decodeFileDescriptor(fd)
+        return b
+    }
+
+    //Todo Take Picture
+    private fun dispatchTakePicture() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(context?.packageManager!!)?.also {
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    null
+                }
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        requireContext(),
+                        "${context?.packageName}.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, CAMERA_REQUEST)
+                }
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File? {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = context?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}",
+            ".jpg",
+            storageDir
+        ).apply {
+            pictureImagePath = absolutePath
+        }
+    }
+
+    fun setImageFileFromCamera() {
+        try {
+            imgFile = File(pictureImagePath)
+            val size = imgFile.length()
+            val s = size / (1024 * 1024)
+            val compress = getCompressSize(s)
+            if (compress == -1) return
+            val bitmap = BitmapFactory.decodeFile(pictureImagePath)
+            val fOut = FileOutputStream(imgFile)
+            bitmap?.compress(Bitmap.CompressFormat.JPEG, compress, fOut)
+            fOut.flush()
+            fOut.close()
+            image_selected.setImageURI(Uri.fromFile(imgFile))
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun getCompressSize(sizeMb: Long): Int {
+        var compressSize = 100
+        when {
+            sizeMb > 10 -> {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.txt_failed_upload_file_size_is_to_large),
+                    Toast.LENGTH_SHORT
+                ).show()
+                return -1
+            }
+            sizeMb > 8 -> {
+                compressSize = 10
+            }
+            sizeMb > 5 -> {
+                compressSize = 20
+            }
+            sizeMb > 2 -> {
+                compressSize = 30
+            }
+            sizeMb > 1 -> {
+                compressSize = 50
+            }
+        }
+        return compressSize
     }
 
     private fun getOpenCamera() {
@@ -155,7 +328,8 @@ class DialogCamera : BaseDialogFragment() {
         val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         val imageFileName = "$timeStamp.jpg"
         val storageDir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES)
+            Environment.DIRECTORY_PICTURES
+        )
         pictureImagePath = storageDir.absolutePath + "/" + imageFileName
         val file = File(pictureImagePath)
         val outputFileUri = Uri.fromFile(file)
@@ -166,22 +340,28 @@ class DialogCamera : BaseDialogFragment() {
     }
 
     private fun getCrop(tempUri: Uri) {
-        UCrop.of(tempUri, Uri.fromFile(File(Globals.getTmpDir(context), UUID.randomUUID().toString() + ".jpg")))
-                .withAspectRatio(16f, 16f)
-                .start((context as Activity))
+        UCrop.of(
+            tempUri,
+            Uri.fromFile(File(Globals.getTmpDir(context), UUID.randomUUID().toString() + ".jpg"))
+        )
+            .withAspectRatio(16f, 16f)
+            .start((context as Activity))
     }
 
     fun getImageContentUri(context: Context, imageFile: File): Uri? {
         val filePath = imageFile.absolutePath
         val cursor = context.contentResolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                arrayOf(MediaStore.Images.Media._ID),
-                MediaStore.Images.Media.DATA + "=? ",
-                arrayOf(filePath), null)
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            arrayOf(MediaStore.Images.Media._ID),
+            MediaStore.Images.Media.DATA + "=? ",
+            arrayOf(filePath), null
+        )
 
         if (cursor != null && cursor.moveToFirst()) {
-            val id = cursor.getInt(cursor
-                    .getColumnIndex(MediaStore.MediaColumns._ID))
+            val id = cursor.getInt(
+                cursor
+                    .getColumnIndex(MediaStore.MediaColumns._ID)
+            )
             val baseUri = Uri.parse("content://media/external/images/media")
             return Uri.withAppendedPath(baseUri, "" + id)
         } else {
@@ -189,7 +369,8 @@ class DialogCamera : BaseDialogFragment() {
                 val values = ContentValues()
                 values.put(MediaStore.Images.Media.DATA, filePath)
                 return context.contentResolver.insert(
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values
+                )
             } else {
                 return null
             }
@@ -203,7 +384,7 @@ class DialogCamera : BaseDialogFragment() {
         return cursor?.getString(idx!!)!!
     }
 
-    fun setCallbak(callback: DialogCameraCallback){
+    fun setCallbak(callback: DialogCameraCallback) {
         callbackDialog = callback
     }
 
